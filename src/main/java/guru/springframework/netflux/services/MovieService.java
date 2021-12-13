8f36f1642dce907006632ba74c1038d283a51532
Alejandro Lopez <alejandro.lopez@lottoland.com>
2021-12-13 15:51:18 +0100

    Added service layer

---
+++ src/main/java/guru/springframework/netflux/services/MovieService.java
@@ -0,0 +1,15 @@
+package guru.springframework.netflux.services;
+
+import guru.springframework.netflux.domain.Movie;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+/**
+ * @author alejandrolopez
+ */
+public interface MovieService {
+
+    Mono<Movie> getMovieById(String id);
+
+    Flux<Movie> getAllMovies();
+}
